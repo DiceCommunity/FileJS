de809ebd309b9f46210e19f1cb03f93052d2092f
DiceCommunity <82623881+DiceCommunity@users.noreply.github.com>
2023-09-27 11:37:36 +0700

    Add files via upload

---
+++ tuser.js
@@ -0,0 +1,8 @@
+function cek() {
+    if (['wendycaarl','49.5Community'].includes(profileq)) {
+        // do something if true
+    } else {
+     // alert("You dont Have Permision From Rich owner, To //run This bot");
+    //    location.reload();
+    }
+}
